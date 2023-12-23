9b9910132f5bd0e88f5646c1daa4f8ad1440ae7b
CJ Pineda <carlojayp.zeniark@gmail.com>
2023-12-23 12:03:44 +0800

    Update at Sat, 23 Dec 2023 04:03:43 GMT

---
+++ pages/competitions/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Header from '../../components/header';
+import c3Logo from '../public/c3Logo.png';
+import './index.css'
+
+
+export default function Competitions() {
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
